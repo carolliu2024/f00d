4bebc45095930046eb7ddf823a70ed312cd2bd6e
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2022-05-28 16:06:27 -0500

    upload image

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import DropDownPicker from 'react-native-dropdown-picker'
+// import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import * as ImagePicker from 'expo-image-picker';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button } from 'react-native';
+// import NativeImagePickerIOS from 'react-native/Libraries/Image/NativeImagePickerIOS';
 
 
 export default function App() {
@@ -14,18 +16,54 @@ export default function App() {
     {label: 'Food3', value: 'f3'}
   ]);
 
+  const options1 = {
+    title: 'Take Image',
+    options: {
+      saveToPhotos: true,
+      mediaType: 'photo',
+    },
+  };
+
+  const options2 = {
+      title: 'Select Image',
+      options: {
+        maxHeight: 200,
+        maxWidth: 200,
+        selectionLimit: 0,
+        mediaType: 'photo',
+      },
+  };
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return(
     <View style={styles.container}>
     <Text style = {styles.title}>Take a picture of your food, and tell us what you ate!</Text>
     <StatusBar style="auto" />
 
     <View style = {styles.imgBox}>
-      <Text>Tap here to select from your image gallery</Text>
+      <Text>Upload your image here!</Text>
     </View>
+    <Button
+      title = "Select from Image Gallery"
+      onPress = {pickImage}
+    />
 
     <Text style={styles.reg}>Select what foods you got, and estimate your portion sizes:</Text>
-
-    
 
     <DropDownPicker
       open = {open}
@@ -78,3 +116,4 @@ const styles = StyleSheet.create({
 });
 
 // export default App;
+
