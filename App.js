a2bef66450859a3da0c158c6f394c3b7a672ef27
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2022-05-28 14:32:17 -0500

    dropdown

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import DropDownPicker from 'react-native-dropdown-picker'
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default function App() {
-  return (
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState([]);
+  const [items, setItems] = useState([
+    {label: 'Food1', value: 'f1'},
+    {label: 'Food2', value: 'f2'},
+    {label: 'Food3', value: 'f3'}
+  ]);
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style = {styles.title}>Take a picture of your food, and tell us what you ate!</Text>
+    <StatusBar style="auto" />
+
+    <View style = {styles.imgBox}>
+      <Text>Tap here to select from your image gallery</Text>
     </View>
+
+    <Text style={styles.reg}>Select what foods you got, and estimate your portion sizes:</Text>
+
+    
+
+    <DropDownPicker
+      open = {open}
+      items={items}
+      value = {value}
+      setOpen = {setOpen}
+      setItems={setItems}
+      setValue = {setValue}
+      // containerStyle={{height: 40}}
+      // defaultIndex={0}
+      multiple = {true}
+    />
+  </View>
   );
+    
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    // textAlign: 'center',
+    marginTop: 50,
+    marginBottom: 10,
+  },
+  reg:{
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  imgBox: {
+    width: '80%',
+    height: '30%',
+    alignItems: 'center',
+    backgroundColor: '#d4d4d4',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    
+  }
 });
+
+// export default App;
