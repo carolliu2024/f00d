2935eb13ff0c4aa7f0a3c4014b4accca6620fa2a
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2022-05-28 21:47:30 -0500

    graph page

--- App.js
+++ App.js
@@ -10,9 +10,10 @@ import axios from 'axios';
 
 
 import React, {useState, useEffect, useRef} from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, Dimensions } from 'react-native';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
+import {PieChart} from 'react-native-chart-kit';
 
 import * as Location from 'expo-location';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -55,7 +56,52 @@ export default function App() {
 const graphPage = () => {
   return (
     <View>
-      <Text>henlo</Text>
+      <Text>Your Nutrition Breakdown</Text>
+      <PieChart
+        data={[
+          {
+            name: 'Protein',
+            population: 21500000,
+            color: 'rgba(131, 167, 234, 1)',
+            legendFontColor: '#7F7F7F',
+            legendFontSize: 15,
+          },
+          {
+            name: 'Carbohydrates',
+            population: 2800000,
+            color: '#F00',
+            legendFontColor: '#7F7F7F',
+            legendFontSize: 15,
+          },
+          {
+            name: 'Fat',
+            population: 8538000,
+            color: '#ffffff',
+            legendFontColor: '#7F7F7F',
+            legendFontSize: 15,
+          },
+        ]}
+        width={Dimensions.get('window').width - 16}
+        height={220}
+        chartConfig={{
+          backgroundColor: '#1cc910',
+          backgroundGradientFrom: '#eff3ff',
+          backgroundGradientTo: '#efefef',
+          decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+        accessor="population"
+        backgroundColor="transparent"
+        paddingLeft="15"
+        absolute //for the absolute number remove if you want percentage
+      />
     </View>
   );
 }
